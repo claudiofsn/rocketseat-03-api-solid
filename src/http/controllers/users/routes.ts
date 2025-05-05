a84347ddfc7a3c61ccc6ac02d98c1bbb3f99b5ae
Cláudio Neto <claudio.neto@alcans.com.br>
2025-05-05 12:56:52 -0300

    feat:criado controllers e rotas para gyms e check-ins

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from "fastify";
+import { authenticate } from "./authenticate";
+import { profile } from "./profile";
+import { register } from "./register";
+import { verifyJWT } from "../../middlewares/verify-jwt";
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post("/users", register);
+  app.post("/sessions", authenticate);
+  /* Autenticado */
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
+}
